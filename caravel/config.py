083bc92e19234ef3f2389efc92993d93923b87ab
Matt King <kingy895@gmail.com>
2016-10-11 12:50:39 -0700

    Remove

--- caravel/config.py
+++ caravel/config.py
@@ -1,5 +1,4 @@
 """The main config file for Caravel
-
 All configuration in this file can be overridden by providing a caravel_config
 in your PYTHONPATH as there is a ``from caravel_config import *``
 at the end of this file.
@@ -43,9 +42,9 @@ CUSTOM_SECURITY_MANAGER = None
 SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
 
 # The SQLAlchemy connection string.
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_DIR, 'caravel.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_DIR, 'caravel.db')
 # SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
-SQLALCHEMY_DATABASE_URI = 'postgresql://kingy:mtk100081@localhost/caravelTest'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@localhost/myapp'
 
 # The limit of queries fetched for query search
 QUERY_SEARCH_LIMIT = 1000
